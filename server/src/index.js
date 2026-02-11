ca8731522a691b840e5765ac5432f350c36a8ff9
Ashish <127130546+akrathor18@users.noreply.github.com>
2026-02-11 11:00:44 +0530

    fix: update CORS config and correct port binding for production

--- server/src/index.js
+++ server/src/index.js
@@ -5,23 +5,26 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import categoryRoutes from "./routes/category.route.js";
 import orderRoutes from "./routes/order.route.js";
-import profileRoutes from "./routes/profile.route.js"
-import cartRoutes from "./routes/cart.route.js"
-import wishlistRoutes from './routes/wishlist.route.js'
+import profileRoutes from "./routes/profile.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import wishlistRoutes from "./routes/wishlist.route.js";
 import cookieParser from "cookie-parser";
 
-import cors from 'cors'
+import cors from "cors";
 const app = express();
 const prisma = new PrismaClient();
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-    ],
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.FRONTEND_URL
+        : "http://localhost:5173",
     credentials: true,
   }),
 );
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.status(201).json("hello world");
@@ -31,15 +34,16 @@ app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/orders", orderRoutes);
 app.use("/user", profileRoutes);
-app.use("/cart", cartRoutes)
-app.use('/wishlist', wishlistRoutes)
+app.use("/cart", cartRoutes);
+app.use("/wishlist", wishlistRoutes);
 
-// deployment 
+// deployment
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+  
